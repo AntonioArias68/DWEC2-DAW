e80eb1b02c5964ba3cd39d8367280ed5235a8ad9
R4f4Lc <rafalc329@gmail.com>
2019-10-23 12:58:18 +0200

    Arreglos finales ejercicio 16

--- Tema 2/Practica 2.5/js/ejercicio16.js
+++ Tema 2/Practica 2.5/js/ejercicio16.js
@@ -1,42 +1,44 @@
-let elementoDiv;
+{
+    let elementoDiv;
 
-function informacion(elEvento) {
-    let posicionVertical, posicionHorizontal;
-    const  coordenadaX = elEvento.clientX;
-    const  coordenadaY = elEvento.clientY;
-    const  [dimensionX, dimensionY] = tamanoVentanaNavegador();
-    if(coordenadaX > dimensionX/2)
-        posicionVertical = "Derecha";
-    
-    else
-        posicionVertical = "Izquierda";
-    
-    if(coordenadaY > dimensionY/2)
-        posicionHorizontal = "Abajo";
+    let informacion = function (elEvento) {
+        let posicionVertical, posicionHorizontal;
+        const  coordenadaX = elEvento.clientX;
+        const  coordenadaY = elEvento.clientY;
+        const  [dimensionX, dimensionY] = tamanoVentanaNavegador();
+        if(coordenadaX > dimensionX/2)
+            posicionVertical = "Derecha";
+        
+        else
+            posicionVertical = "Izquierda";
+        
+        if(coordenadaY > dimensionY/2)
+            posicionHorizontal = "Abajo";
 
-    else
-        posicionHorizontal = "Arriba";
-    
-    return muestraInformacion(['Posicion', posicionHorizontal, posicionVertical]);
+        else
+            posicionHorizontal = "Arriba";
+        
+        return muestraInformacion(['Posicion', posicionHorizontal, posicionVertical]);
+    };
+
+    let muestraInformacion = function(mensaje) {
+        let texto = '<h1>'+mensaje[0]+'</h1>';
+        for(var i=1; i<mensaje.length; i++) {
+            texto += '<p>'+mensaje[i]+'</p>';
+        }
+        elementoDiv.innerHTML = texto;
+    };
+
+    let tamanoVentanaNavegador = function(){
+        return [window.innerWidth, window.innerHeight];
+    };
+
+    let init = function(){
+        elementoDiv = document.getElementById("info");
+
+        document.addEventListener("click", informacion);
+    };
+
+    document.addEventListener("DOMContentLoaded", init);
+
 }
-function muestraInformacion(mensaje) {
-    let texto = '<h1>'+mensaje[0]+'</h1>';
-    for(var i=1; i<mensaje.length; i++) {
-        texto += '<p>'+mensaje[i]+'</p>';
-    }
-    elementoDiv.innerHTML = texto;
-}
-function tamanoVentanaNavegador(){
-    return [window.innerWidth, window.innerHeight];
-}
-
-let inicio = function(){
-    elementoDiv = document.getElementById("info");
-
-    document.addEventListener("click", informacion);
-};
-
-document.addEventListener("DOMContentLoaded", inicio);
-
-
-//document.onclick = informacion;
